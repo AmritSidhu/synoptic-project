6884a0ac4fea2bd1b975cb5a9c960589edda694f
AmritSidhu <amrit.sidhu@digital.cabinet-office.gov.uk>
2019-05-29 11:53:56 +0100

    Finished

--- synoptic-project/app/models/user.rb
+++ synoptic-project/app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   before_save { self.email = email.downcase }
   validates :name, presence: true
   validates :email, presence: true
+  validates :employeeid, presence: true
+  validates :mobile, presence: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
   uniqueness: true
